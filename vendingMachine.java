facfa5b4502e65a1052354b77698c5ebd30754f7
Fahim <fahim@Fahims-MacBook-Pro-5.local>
2025-02-12 19:32:34 -0500

    Added the price of the snacks

---
+++ vendingMachine.java
@@ -0,0 +1,30 @@
+public class VendingMachine {
+
+	public static void main(String[] args) {
+		
+		String snickers;
+		String sourPatchKids;
+		String laysOriginal;
+		String doritos;
+		String cookies;
+		String strawberryPoptarts;
+		String granolaBar;
+		String chexMix;
+		String beefJerky;
+		String swedishFish;
+
+		float priceSnickers = 1.29f;
+		float priceSourpatchkids = 1.89f;
+		float priceLaysoriginal = 0.89f;
+		float priceDoritos = 0.89f;
+		float priceCookies = 2.59f;
+		float pricestrawberrypoptarts = 4.19f;
+		float priceGranolabar = 2.99f;
+		float priceChexMixs = 8.19f;
+		float priceBeefjerky = 9.29f;
+		float priceSwedishfish = 0.50f;
+
+
+		
+	}
+}
