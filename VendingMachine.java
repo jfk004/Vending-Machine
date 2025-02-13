e762d44f8281c3c07cdd71d40218c0869839e078
WilsonC67 <wilson.chen@quinnipiac.edu>
2025-02-12 19:17:02 -0500

    Added some snacks as Strings.

---
+++ VendingMachine.java
@@ -0,0 +1,12 @@
+public class VendingMachine {
+	private String snickers;
+	private String sourPatchKids;
+	private String laysOriginal;
+	private String doritos;
+	private String cookies;
+	private String strawberryPoptarts;
+	private String granolaBar;
+	private String chexMix;
+	private String beefJerky;
+	private String swedishFish;
+}
